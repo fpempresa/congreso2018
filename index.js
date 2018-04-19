6df29ec1dbe50ae1312fcc0e11f7aedea4b41329
Lorenzo Gonzalez <logongas@gmailcom>
2018-04-19 13:43:08 +0200

    Version inicial del sitio

---
+++ index.js
@@ -0,0 +1,12 @@
+"user strict";
+
+var app = angular.module('app', []);
+
+app.controller("IndexController", ['$scope', 'animateScroll', function ($scope, animateScroll) {
+        $scope.animateScroll = animateScroll;
+        
+
+        
+
+        
+}]);
